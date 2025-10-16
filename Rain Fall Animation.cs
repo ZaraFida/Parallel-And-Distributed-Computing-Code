7249666938b4ae0e744483ae9cec4173bfa2a0af
Zara Fida <b6309622@gmail.com>
2025-10-16 12:26:25 +0500

    Add files via upload

---
+++ Rain Fall Animation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+class RainAnimation
+{
+    static void Main()
+    {
+        string[] frames = new string[]
+        {
+            @"
+   |     |       |     |
+      |       |     |    
+   |     |       |     |
+",
+            @"
+      |       |     |    
+   |     |       |     |
+      |       |     |    
+",
+            @"
+   |     |       |     |
+      |       |     |    
+   |     |       |     |
+"
+        };
+        Console.CursorVisible = false;
+        for (int i = 0; i < 50; i++)
+        {
+            foreach (string frame in frames)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(frame);
+                Thread.Sleep(300);
+            }
+        }
+        Console.CursorVisible = false;
+    }
+}
