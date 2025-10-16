0c0d2bb2adf4f781575ba92278c18b9ea974a34d
Zara Fida <b6309622@gmail.com>
2025-10-16 13:27:09 +0500

    Add files via upload

---
+++ Client Side(User,Password).cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Net.Sockets;
+using System.Text;
+class AuthClient
+{
+    static void Main()
+    {
+
+        var c = new TcpClient("10.10.20.18", 5000);
+        var ns = c.GetStream();
+        Console.Write("Username:");
+        string user = Console.ReadLine();
+        Console.Write("Password:");
+        string pas = Console.ReadLine();
+        ns.Write(Encoding.UTF8.GetBytes($"{user},{pas}"));
+        byte[] buf = new byte[256];
+        int n = ns.Read(buf);
+        Console.WriteLine("Server Response:" + Encoding.UTF8.GetString(buf, 0, n));
+
+
+    }
+}
---
+++ Server Side(User and password).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+class AuthServer
+{
+    static void Main()
+    {
+        var users = new Dictionary<string, string>
+        {
+            {"admin","1234"},{"user","pass"},{"test","1111"} 
+        };
+        var s = new TcpListener(IPAddress.Any, 5000);
+        s.Start();
+        Console.WriteLine(" Auth Server Running...");
+        while (true)
+        {
+            using var c = s.AcceptTcpClient();
+            using var ns = c.GetStream();
+            byte[] buf = new byte[256];
+            int n= ns.Read(buf);
+            string[] creds = Encoding.UTF8.GetString(buf, 0, n).Split(',');
+            string user=creds[0],pass=creds[1];
+            string msg= users.ContainsKey(user)&&users[user]==pass?"Login Successful":"Invalid Credentials";
+                ns.Write(Encoding.UTF8.GetBytes(msg));
+                Console.WriteLine($"{user}->{msg}");
+            }
+        }
+    }
