1464a9f81f4cc91604510ab51f6779a74a3c24a4
Zara Fida <b6309622@gmail.com>
2025-10-16 12:25:36 +0500

    Add files via upload

---
+++ Client Side(Int number).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+class Client
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var client = new TcpClient("10.10.20.18", 5000);
+            var ns = client.GetStream();
+            Console.Write("Enter Number:");
+            string msg = Console.ReadLine();
+            ns.Write(Encoding.UTF8.GetBytes(msg));
+            byte[] data = new byte[4096];
+            int n = ns.Read(data, 0, data.Length);
+            Console.WriteLine("Server Reply:" + Encoding.UTF8.GetString(data, 0, n));
+            ns.Close();
+            client.Close();
+        }
+    }
+}
